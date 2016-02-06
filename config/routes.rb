13e3e901d7ea2c1ee0362c6f83c38aa694c8804b
David Stinnette <dastinnette@gmail.com>
2016-02-06 16:29:27 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/about", to: "static#about"
-  # put "/status" to: 
 
   root "static#home"
 end
