a6226797f41f2f70895ef40a8ebb28cfb62df2ec
Steven Olson <steveoscaro@gmail.com>
2016-02-08 19:48:40 -0700

    made the featured dream on the bottom of the categories index unique and dynamic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    #random cause for featured cause on bottom of page
+    @cause = Cause.find(rand(Cause.all.pluck(:id).sample))
   end
 
   def show
