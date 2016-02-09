00aefeb40538eeff231d2465f1281296ca82f790
James Crockett <jecrockett@gmail.com>
2016-02-09 16:12:10 -0700

    Add featured images to assets and update homepage nav colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -154,7 +154,7 @@ class Seed
     Cause.create!(
     title: "Squirrel Census",
     description: "Those furry little bastards have flown under the radar for far too long",
-    image_url: "https://wallpaperscraft.com/image/skrat_squirrel_ice_age_ice_falling_63305_3840x1200.jpg",
+    image_url: "/assets/squirrel_pic.jpg",
     goal: 25000,
     user_id: User.find_by(email: "mike_dao@dreambuilder.com").id,
     category_id: Category.pluck(:id).sample,
@@ -165,7 +165,7 @@ class Seed
     Cause.create!(
     title: "Ostrich Pillow",
     description: "Get your nap on anytime, anywhere",
-    image_url: "http://www.crowdfunding-shop.com/uploads/1/1/8/9/11893290/s127856143605570547_p8_i1_w640.jpeg",
+    image_url: "/assets/ostrich-pillow.jpeg",
     goal: 3800,
     user_id: User.find_by(email: "mike_dao@dreambuilder.com").id,
     category_id: Category.pluck(:id).sample,
