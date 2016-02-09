0f1e68c67f37f3b662789c8479f201404279b0cc
Steven Olson <steveoscaro@gmail.com>
2016-02-08 19:58:37 -0700

    controller bug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
     #random cause for featured cause on bottom of page
-    @cause = Cause.find(rand(Cause.all.pluck(:id).sample))
+    @cause = Cause.all.first
   end
 
   def show
